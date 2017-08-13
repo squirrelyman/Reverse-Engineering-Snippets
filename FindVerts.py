5769b26c397a3a47ba68686fb566b3ba2ff109a8
squirrelyman <30965378+squirrelyman@users.noreply.github.com>
2017-08-12 20:56:20 -0400

    Initial commit.

---
+++ FindVerts.py
@@ -0,0 +1,20 @@
+import struct
+
+def isInRange(x):
+    return abs(x) > 0.01 and abs(x) < 1000
+
+path = "C:/Users/joehe/Documents/Visual Studio 2015/Projects/NFSLoader/NFS Hacks/Cars/A3/Geometry.bin"
+
+with open(path, 'rb') as f:
+    data = f.read()
+
+#don't read more than 100kb of data, so this thing in't spitting out text forever.
+dataLen = min(100 * 1024, len(data))
+
+for index in range(0, dataLen - 8, 4):
+    x = struct.unpack('f', data[index+0:index+4])[0]
+    y = struct.unpack('f', data[index+4:index+8])[0]
+    z = struct.unpack('f', data[index+8:index+12])[0]
+
+    if isInRange(x) and isInRange(y) and isInRange(z):
+        print('({}, {}, {}) @ {:X}'.format(x, y, z, index))
